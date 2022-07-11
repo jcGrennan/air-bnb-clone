6bbec5bec1234f0f4ecb0702a8ff20f38172261e
JC Grennan <jgrennan94@gmail.com>
2022-07-11 12:15:57 +0100

    Hero component set-up

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import grid from "../images/airbnb-grid.png"
+
+export default function Hero() {
+    return (
+        <div>
+            <img src={grid} alt="people performing skills or hobbies"></img>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
