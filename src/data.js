140cb03d6045a422201bb83479d55b6dbe361df4
JC Grennan <jgrennan94@gmail.com>
2022-08-19 14:41:50 +0100

    fixed eslint warning. assigned data to a variable

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 // all the images for the card experiences.
+
 import weightlifting from "./images/alana-grose-weightlifting.jpg"
 import painting from "./images/adi-ware-painting.jpg"
 import cello from "./images/andy-zampf-cello.jpg"
@@ -8,7 +9,8 @@ import graphicDesign from "./images/bill-ketters-graphic-design.jpg"
 /* exporting the card data. Normally this would be pulled from a database in JSON format.
 For the sake of this exercise I created a file and put the data into objects contained within an array,
 similar to JSON so that I could map the data onto my reusuable card component.*/
-export default [
+
+const data= [
     {
         id: 1,
         title: "Weightlifting with Alana Grose",
@@ -75,3 +77,5 @@ export default [
         openSpots: 5,
     },
 ]
+
+export default data
