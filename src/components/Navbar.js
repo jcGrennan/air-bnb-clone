3e2f0a3199a73afdbc542ea63d052e805e9dcfac
JC Grennan <jgrennan94@gmail.com>
2022-08-19 14:45:19 +0100

    comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
+// importing airBnb logo
+
 import logo from "../images/airbnb-logo.png"
+
+// composing and exporting the Navbar. Very small but composed separately for practice
 
 export default function Navbar() {
     return (
