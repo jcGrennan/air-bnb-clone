20c193ba8a32b9157a26af563ef274771781786c
JC Grennan <jgrennan94@gmail.com>
2022-08-19 14:44:08 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // importing all the components and also the data file for the card info.
+
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
 
-function App() {
+export default function App() {
 
   // using the map method to map the data of the individual experiences into card components held within a variable 'cards'
+  
   const cards = data.map(item => {
       return (
           <Card
@@ -28,5 +30,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
